3690945763fa61c5899321ae45335a95f138c08d
thammaraku <thammaraku@gmail.com>
2020-11-03 13:56:48 -0600

    Summary of changes - add function to show question and choice - add function to compare user answer with correct answer - add fucntion to get element by ID - add enventlisterner to render question every time the page load - add question array of objects

--- script.js
+++ script.js
@@ -1,53 +1,67 @@
+
+// functions needed
+// dom target element
+// display question and choices
+// compare user pick with correct answer
+
+
 var pos = 0;
 var correct = 0;
-var test, test_status, question,choice, choices, chA, chB, chC;
+var choice = "";
+var choices = "";
+
+var quiz, quizStatus, question, choice, choices, chA, chB, chC;
 
 // keep questions as objects in an array
 const myQuestions = [
     {
         question: "Who invented JavaScript?",
-        answer: {
-            a: "Lebron James"
-            b: "Ronaldo"
-            c: "Brendan Eich"
-        },
-        correctAnswer: "c"
+        a: "Lebron James",
+        b: "Ronaldo",
+        c: "Brendan Eich",
+        answer: "c"
     },
     {
         question: "Which one of these is a JavaScript package manager?",
-        answer: {
-            a: "bpm"
-            b: "npm"
-            c: "abc"
-        },
-        correctAnswer: "b"
-    }
+        a: "bpm",
+        b: "npm",
+        c: "abc",
+        answer: "b"
+    },
     {
         question: "Which tool can you use to ensure code quality?",
-        answer: {
-            a: "Angular"
-            b: "Angelo"
-            c: "jQuery"
-            d: "all correct"
-        },
-        correctAnswer: "a"
+        a: "Angular",
+        b: "Angelo",
+        c: "jQuery",
+        d: "all correct",
+        answer: "a"
     }
 ];
 
 
-function get() {
+function get(x) {
     return document.getElementById(x);
 }
 
 function renderQuestion() {
 
     // show question and choices on the page
-    test = get("test");
+    quiz = get("quiz");
 
+    console.log("quiz= " + quiz);
+    console.log("pos= " + pos);
+    console.log("myQuestions.length= " + myQuestions.length);
+    console.log("correct= " + correct);
+
+    // test completed when current postion more then question array
     if(pos >= myQuestions.length) {
 
-        test.innerHTML = "<h2>You got "+correct+" of "+questions.length+" questions correct</h2>";
-        get("test_status").innerHTML = "Test completed";
+        console.log(pos);
+        console.log(myQuestions.length);
+        console.log(correct);
+
+        quiz.innerHTML = "<h2>You got "+correct+" of "+myQuestions.length+" questions correct</h2>";
+        get("quizStatus").innerHTML = "Test completed";
 
         pos = 0;
         correct = 0;
@@ -55,18 +69,63 @@ function renderQuestion() {
         return false
     }
 
-    get ("test_status").innerHTML = "Question " + (pos+1) + " of " + question.length;
+    // display question
+    get ("quizStatus").innerHTML = "Question " + (pos+1) + " of " + myQuestions.length;
 
     question = myQuestions[pos].question;
     chA = myQuestions[pos].a;
     chB = myQuestions[pos].b;
     chC = myQuestions[pos].c;
 
-    test.innerHTML = "<h3>" + question + "</h3>";
-
-    test.innerHTML += "<label> <input type='radio' name='choices' value='A'> "+chA+"</label><br>";
-    test.innerHTML += "<label> <input type='radio' name='choices' value='B'> "+chB+"</label><br>";
-    test.innerHTML += "<label> <input type='radio' name='choices' value='C'> "+chC+"</label><br><br>";
-    test.innerHTML += "<button onclick='checkAnswer()'>Submit Answer</button>";
-
+    // dispay choices
+    quiz.innerHTML = "<h3>" + question + "</h3>";
+    // showing choices using addition assignment operation mean quiz.innerHTML= quiz.innerHTML + radio + chA
+    // radio button must have the same name which allow only one to select
+    quiz.innerHTML += "<label> <input type='radio' name='choices' value='a'> "+chA+"</label><br>";
+    quiz.innerHTML += "<label> <input type='radio' name='choices' value='b'> "+chB+"</label><br>";
+    quiz.innerHTML += "<label> <input type='radio' name='choices' value='c'> "+chC+"</label><br><br>";
+    quiz.innerHTML += "<button onclick='checkAnswer()'>Submit Answer</button>";
 }
+
+
+function checkAnswer() {
+
+    choices = document.getElementsByName("choices");
+    console.log("choices= " + choices);
+
+    for(var i = 0; i < choices.length; i++) {
+        if (choices[i].checked) {            
+            console.log("choices[i].checked= " + choices[i].checked);
+            console.log("choices= " + choices);
+
+            choice = choices[i].value;
+            console.log("choice= " + choice);
+            console.log("choices[i].value= " + choices[i].value);
+
+        }
+    }
+
+    console.log("choice= " + choice);
+    console.log("myQuestions[pos].answer= " + myQuestions[pos].answer);
+    // check if choice match with correct key answer from array
+    if(choice == myQuestions[pos].answer) {
+        // increase user score.
+        correct++;
+    } else {
+        // place holder to add reduce timer if wrong
+        // correct--;
+    }
+
+    // increase position to next question
+    pos++;
+    renderQuestion();
+}
+
+// add evenlistener to show question when page load
+
+
+
+
+
+
+window.addEventListener("load", renderQuestion);
